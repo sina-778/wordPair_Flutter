56dc0094bbf557e1f5429354bd5bca9082e1485d
Ebna Sina <isina413.is@gmail.com>
2022-04-26 14:53:02 +0600

    Demo of 26-04-22

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    final wordPair = WordPair.random();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.light().copyWith(
        appBarTheme: AppBarTheme(color: Colors.deepPurple)
       ),
-      home: RandomWord()
-    );
+      home: RandomWord());
   }
 
 }
+
+
+
